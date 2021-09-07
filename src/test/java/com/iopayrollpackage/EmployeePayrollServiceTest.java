66360ab6b4a6783206ab2a6f5a5717d47a3142e4
SRC0747 <RjSrc@0747>
2021-09-07 13:49:58 +0530

    Welcome to Employee Payroll Service System

---
+++ src/test/java/com/iopayrollpackage/EmployeePayrollServiceTest.java
@@ -0,0 +1,10 @@
+package com.iopayrollpackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeePayrollServiceTest {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Payroll Service System");
+    }
+
+}
